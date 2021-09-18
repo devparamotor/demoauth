c33b01feb652e351e2d3a2deb805a53fb55270e2
rishav <support@paramotordt.com>
2021-09-18 13:23:10 +0530

    Second Commit

--- authuser/settings.py
+++ authuser/settings.py
@@ -77,17 +77,22 @@ WSGI_APPLICATION = 'authuser.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bankifyu',
+#         'USER': 'postgres',
+#         'PASSWORD': '2201',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bankifyu',
-        'USER': 'postgres',
-        'PASSWORD': '2201',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
